3e17f725a42457d27f15447e01794b6ac976ce38
Eldrex Delos Reyes Bula <eldrexdelosreyesbula@gmail.com>
2025-12-29 23:20:39 +0800

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,13 @@
-// firebase-config.js
+// js/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
 import { 
     getAuth, 
     signInAnonymously, 
-    signOut, 
+    signOut,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
     onAuthStateChanged 
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 import { 
@@ -14,23 +17,23 @@ import {
     where,
     orderBy,
     limit,
+    startAfter,
     getDocs,
     doc,
     getDoc,
+    setDoc,
     updateDoc,
-    arrayUnion,
-    arrayRemove,
-    onSnapshot,
-    addDoc,
     deleteDoc,
-    serverTimestamp
+    addDoc,
+    serverTimestamp,
+    Timestamp,
+    runTransaction
 } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-firestore.js";
 
-// Firebase configuration
+// Firebase Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyChd3wKk1KXZNZQs8fDZRiUFbelciQnT1w",
     authDomain: "eldrex-blog.firebaseapp.com",
-    databaseURL: "https://eldrex-blog-default-rtdb.firebaseio.com",
     projectId: "eldrex-blog",
     storageBucket: "eldrex-blog.firebasestorage.app",
     messagingSenderId: "1016235801394",
@@ -43,6 +46,7 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const googleProvider = new GoogleAuthProvider();
 
 // Export Firebase services
 export {
@@ -50,22 +54,30 @@ export {
     analytics,
     auth,
     db,
+    googleProvider,
+    // Auth Methods
     signInAnonymously,
+    signInWithEmailAndPassword,
+    signInWithPopup,
     signOut,
     onAuthStateChanged,
+    // Firestore Methods
     collection,
     query,
     where,
     orderBy,
     limit,
+    startAfter,
     getDocs,
     doc,
     getDoc,
+    setDoc,
     updateDoc,
-    arrayUnion,
-    arrayRemove,
-    onSnapshot,
-    addDoc,
     deleteDoc,
-    serverTimestamp
+    addDoc,
+    serverTimestamp,
+    Timestamp,
+    runTransaction,
+    // Other
+    GoogleAuthProvider
 };
